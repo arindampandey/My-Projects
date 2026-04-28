74fb3057582cc8d7e265a624e90913f330bb5f7e
Arindam Pandey <arindampandey63@gmail.com>
2026-04-28 12:04:47 +0530

    Update Navbar.jsx

--- TodolistApp/src/components/Navbar.jsx
+++ TodolistApp/src/components/Navbar.jsx
@@ -6,9 +6,7 @@ const Navbar = () => {
             <div className="logo">
                 <span>Task_Reminder - Manage your tasks at one place</span>
             </div>
-            {/* <li className='lis'>Home</li>
-            <li className='lis'>Your Tasks</li> */}
-
+        
         </nav>
     )
 }
